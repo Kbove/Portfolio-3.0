2252694207ad2a7eac0ce0f889260985b5d2872e
Kbove <kbove94@gmail.com>
2022-01-12 21:02:46 -0800

    heroku pushing

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -25,7 +25,7 @@ export default function PortfolioContainer() {
         <div style={{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
             <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
             {renderPage()}
-            <footer>
+            <footer style={{paddingTop: '30px'}}>
                 <p style={{textAlign: 'center', marginTop: '20px', color: 'white', backgroundColor: 'black', margin: 'auto'}}>Made with love by Kyle using React.js. Please contact me at <a style={{fontWeight: 'bold', color: 'white'}}href="mailto:kbove94@gmail.com">kbove94@gmail.com</a></p>
             </footer>
         </div>
